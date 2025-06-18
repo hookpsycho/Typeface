4f1ffd0dee7e1f56adfb14081b88a076207c7088
0hook <136810334+0hook@users.noreply.github.com>
2025-06-18 18:40:51 -0400

    -

--- Register.lua
+++ Register.lua
@@ -1,4 +1,4 @@
-local Library = {  }
+local Library = {}
 
 -- // Assert Alternative
 Library.Throw = function(Condition, String)
@@ -6,105 +6,102 @@ Library.Throw = function(Condition, String)
 end
 
 --[[ Compatibility Check ]] do
-    cloneref                = cloneref or function(service) return service end
+    cloneref = cloneref or function(service) return service end
 
-    isfile                  = Library.Throw(isfile, 'Executor is incompatible!')
-    isfolder                = Library.Throw(isfolder, 'Executor is incompatible!')
-    writefile               = Library.Throw(writefile, 'Executor is incompatible!')
-    makefolder              = Library.Throw(makefolder, 'Executor is incompatible!')
-    getcustomasset          = Library.Throw(getcustomasset, 'Executor is incompatible!')
+    isfile = Library.Throw(isfile, 'Executor is incompatible!')
+    isfolder = Library.Throw(isfolder, 'Executor is incompatible!')
+    writefile = Library.Throw(writefile, 'Executor is incompatible!')
+    makefolder = Library.Throw(makefolder, 'Executor is incompatible!')
+    getcustomasset = Library.Throw(getcustomasset, 'Executor is incompatible!')
 end
 
 -- // Metamethods
 Library.__index = Library
 
 -- // Services
-local Http = cloneref(game:GetService 'HttpService')
+local Http = cloneref(game:GetService("HttpService"))
 
 -- // Tables
-Library.Fonts = { }
+Library.Fonts = {}
 
 -- // Functions
 function Library:Register(Info)
-    Info = setmetatable(Info or { }, Library)
+    Info = setmetatable(Info or {}, Library)
 
-    Info.url = Library.Throw(Info.url, 'Url is missing!')
-    Info.name = Library.Throw(Info.name or self.name, 'Name is missing!')
+    Info.Url = Library.Throw(Info.Url, 'Url is missing!')
+    Info.Name = Library.Throw(Info.Name or self.Name, 'Name is missing!')
 
-    Info.weight = Info.weight or 'Regular'
-    Info.style = Info.style or 'Normal'
+    Info.Weight = Info.Weight or 'Regular'
+    Info.Style = Info.Style or 'Normal'
 
-    Info.path = Library.Throw(Info.path or Library.Path, 'Path is missing!')
-    Info.enumweight = Library.Throw(Enum.FontWeight[Info.weight], `Couldn't find enum weight for "{Info.weight}"!`)
-    Info.numweight = Library.Throw(Info.enumweight.Value, `Couldn't find numerical weight for "{Info.weight}"!`)
-    Info.enumstyle = Library.Throw(Enum.FontStyle[Info.style], `Couldn't find enum style for "{Info.style}"!`)
+    Info.Path = Library.Throw(Info.Path or Library.Path, 'Path is missing!')
+    Info.EnumWeight = Library.Throw(Enum.FontWeight[Info.Weight], `Couldn't find enum weight for "{Info.Weight}"!`)
+    Info.NumWeight = Library.Throw(Info.EnumWeight.Value, `Couldn't find numerical weight for "{Info.Weight}"!`)
+    Info.EnumStyle = Library.Throw(Enum.FontStyle[Info.Style], `Couldn't find enum style for "{Info.Style}"!`)
 
-    Info.fullname = `{ Info.numweight ~= '400' and '-' .. Info.weight or '' }{ Info.style ~= 'Normal' and '-' .. Info.style or '' }`
-    Info.fontpath = `{ Info.path }\\{ Info.name }{ Info.fullname }.ttf`
-    Info.familypath = `{ Info.path }\\Families`
+    Info.FullName = `{ Info.NumWeight ~= '400' and '-' .. Info.Weight or '' }{ Info.Style ~= 'Normal' and '-' .. Info.Style or '' }`
+    Info.FontPath = `{ Info.Path }\\{ Info.Name }{ Info.FullName }.ttf`
+    Info.FamilyPath = `{ Info.Path }\\Families`
+    Info.JsonPath = `{ Info.FamilyPath }\\{ Info.Name }.json`
 
-    Info.jsonpath = `{ Info.familypath }\\{ Info.name }.json`
+    Info.Family = self.Family
 
-    Info.family = self.family
-
-    Library:CheckPath(Info.path)
-    Library:CheckPath(Info.familypath)
+    Library:CheckPath(Info.Path)
+    Library:CheckPath(Info.FamilyPath)
 
     Info:CreateFont()
     Info:CreateFamily()
 
-    Library.Fonts[Info.name .. Info.fullname] = Info
-    Library.Path = Info.path
+    Library.Fonts[Info.Name .. Info.FullName] = Info
+    Library.Path = Info.Path
 
     return Info:Get(), Info
 end
 
 function Library:Get(Name)
-    local face = Library.Fonts[Name] or self
-
-    return Font.new(getcustomasset(face.jsonpath), face.enumweight, face.enumstyle)
+    local Face = Library.Fonts[Name] or self
+    return Font.new(getcustomasset(Face.JsonPath), Face.EnumWeight, Face.EnumStyle)
 end
 
 function Library:CheckPath(Path)
     if isfolder(Path) then return end
-
     makefolder(Path)
 end
 
 function Library:CreateFont()
     local Info = Library.Throw(self, 'Information is missing!')
 
-    if isfile(Info.fontpath) then return end
+    if isfile(Info.FontPath) then return end
 
     local Success, Result = pcall(function()
-        return game:HttpGet(Info.url)
+        return game:HttpGet(Info.Url)
     end)
 
-    Library.Throw(Success, `Failed to fetch "{ Info.name }-{ Info.weight }-{ Info.style }" file!`)
-    warn(`Successfully fetched "{ Info.name }-{ Info.weight }-{ Info.style }" file!`)
+    Library.Throw(Success, `Failed to fetch "{Info.Name}-{Info.Weight}-{Info.Style}" file!`)
+    warn(`Successfully fetched "{Info.Name}-{Info.Weight}-{Info.Style}" file!`)
 
-    writefile(Info.fontpath, Result)
+    writefile(Info.FontPath, Result)
 end
 
 function Library:CreateFamily()
     local Info = Library.Throw(self, 'Information is missing!')
 
-    Info.family = Info.family or {
-        name = Info.name,
-        faces = { }
+    Info.Family = Info.Family or {
+        name = Info.Name,
+        faces = {}
     }
 
-    Info.fullname = string.split(Info.fullname, '-')
-    Info.fullname = `{ Info.fullname[2] }{ Info.fullname[3] and ' ' .. Info.fullname[3] or '' }`
+    Info.FullName = string.split(Info.FullName, '-')
+    Info.FullName = `{ Info.FullName[2] }{ Info.FullName[3] and ' ' .. Info.FullName[3] or '' }`
 
-    table.insert(Info.family.faces, {
-        name = Info.fullname,
-        weight = Info.numweight,
-        style = string.lower(Info.style),
-        assetId = getcustomasset(Info.fontpath),
+    table.insert(Info.Family.faces, {
+        name = Info.FullName,
+        weight = Info.NumWeight,
+        style = string.lower(Info.Style),
+        assetId = getcustomasset(Info.FontPath),
     })
 
-    writefile(Info.jsonpath, Http:JSONEncode(Info.family))
+    writefile(Info.JsonPath, Http:JSONEncode(Info.Family))
 end
 
 return Library
